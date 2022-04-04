428fc6a6fe3dc26c355ccf4209770efa0103f8ac
unknown <soran.taak@gmail.com>
2022-04-05 00:28:29 +0430

    add news and video section

--- components/news/News.jsx
+++ components/news/News.jsx
@@ -5,7 +5,7 @@ function News() {
     <section className="pb-20">
       <div className="w-4/5 container mx-auto">
         <h2 className="text-2xl py-10 text-center">آخرین اخبار</h2>
-        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 space-y-5">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-10">
           <NewsItem />
           <NewsItem />
           <NewsItem />
--- components/news/NewsItem.jsx
+++ components/news/NewsItem.jsx
@@ -2,7 +2,7 @@ import Image from "next/dist/client/image";
 import Link from "next/dist/client/link";
 function NewsItem() {
   return (
-    <div className="grid grid-cols-3 rounded-sm overflow-hidden shadow-md">
+    <div className="grid grid-cols-1 sm:grid-cols-3 rounded-sm overflow-hidden shadow-md">
       <div className="col-span-1">
         <Image
           src="/image/dentist.jpg"
@@ -11,14 +11,14 @@ function NewsItem() {
           height={5462}
         />
       </div>
-      <div className="col-span-2 px-2">
+      <div className="col-span-1 sm:col-span-2 px-2">
         <h3 className="text-lg font-bold text-textParColor">عنوان خبر</h3>
-        <p className="text-sm text-justify text-textParColor">
+        <p className="text-md lg:text-lg text-justify text-textParColor">
           لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
           از طراحان گرافیک است. .
         </p>
         <div className="flex flex-row justify-end">
-          <span className="text-darkGold hover:text-brightGold text-sm">
+          <span className="text-darkGold hover:text-brightGold text-lg pb-2 md:pb-2 ">
             <Link href="#">بیشتر بخوانید</Link>
           </span>
         </div>
