bacbd65bb3aac075c81612bd7c5ebf8dfecaa22e
unknown <soran.taak@gmail.com>
2022-05-02 00:27:08 +0430

    add user profile picture

--- components/dashboard/userAndSearch/user-search.jsx
+++ components/dashboard/userAndSearch/user-search.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import SearchDashboard from "./search-dashboard";
+import UserAvatar from "./user-avatar";
 
 function UserAndSearch() {
   return (
-    <div className=" container">
-      <div className="mx-auto py-2 w-10/12">
+    <div className=" container w-4/5 mx-auto">
+      <div className="py-2 flex flex-row justify-between">
         <SearchDashboard />
+        <UserAvatar />
       </div>
     </div>
   );
