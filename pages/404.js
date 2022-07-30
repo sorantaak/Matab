d7e56e7af9483bb1f63eb6a4567095bd011cc2c0
soran <soran.taak@gmail.com>
2022-07-30 20:14:40 +0430

    change 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,21 +1,24 @@
-import React from 'react'
-import Image from 'next/image'
-import Row from '../components/ui/row/Row'
+import React from "react";
+import Image from "next/image";
+import Row from "../components/ui/row/Row";
 function PageNotFound() {
-    return (
-        <Row>
-            <div className='flex flex-row justify-center items-center gap-20 pt-5'>
-                <div className='flex flex-col justify-center items-center'>
-                    <h4 className='text-[200px] text-darkGold'>404</h4>
-
-                </div>
-                <div className='h-80 w-80'>
-                    <Image src="/image/logo.png" width={900} height={679} layout="responsive" />
-                </div>
-
-            </div>
-        </Row>
-    )
+  return (
+    <Row>
+      <div className="flex flex-col sm:flex-row justify-center items-center gap-20 pt-5">
+        <div className="flex flex-col justify-center items-center">
+          <h4 className="text-[200px] text-darkGold">404</h4>
+        </div>
+        <div className="h-80 w-80">
+          <Image
+            src="/image/logo.png"
+            width={900}
+            height={679}
+            layout="responsive"
+          />
+        </div>
+      </div>
+    </Row>
+  );
 }
 
-export default PageNotFound
+export default PageNotFound;
